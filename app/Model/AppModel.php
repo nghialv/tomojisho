24a2be0f03befc1937ce1b049cd4c8694deada3c
Le Van Nghia <nghialv2607@gmail.com>
2012-12-18 16:27:41 +0900

    test

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -31,29 +31,4 @@ App::uses('Model', 'Model');
  * @package       app.Model
  */
 class AppModel extends Model {
-  var $facebook;
-  var $fb_user;
-
-  function __construct($request=null, $response=null) {
-    parent::__construct($request, $response);
-    App::import('Vendor', 'facebook/src/facebook');
-    $this->facebook = new Facebook(array(
-            'appId' => Configure::read('Facebook.appId'),
-            'secret' => Configure::read('Facebook.secret'),
-            'cookie' => true,
-     ));
-  }
-
-  public function beforeFilter(){
-    $this->fb_user = $this->facebook->getUser();
-
-    if(empty($this->fb_user)) {
-      $this->redirect($this->facebook->getLoginUrl(array(
-      'scope' => Configure::read('Facebook.scope'),
-      'redirect_uri' => Configure::read('Facebook.appUrl')
-      )));
-    } else {
-      $fb_user_info = $this->facebook->api('/'.$this->fb_user);
-    }
-  }
 }
