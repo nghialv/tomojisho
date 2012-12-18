324d90ce77fcafb58748b1f54804e0b0bb4cff66
dxhuy1988 <doxuanhuy@gmail.com>
2012-12-19 01:14:30 +0900

    game logic

--- app/Controller/GameController.php
+++ app/Controller/GameController.php
@@ -5,7 +5,6 @@ App::uses('AppController', 'Controller');
 
 class GameController extends AppController {
   var $FB;
-
   /*
   public function __contruct($resquest=null, $respond=null)
   {
@@ -13,6 +12,7 @@ class GameController extends AppController {
     $this->FB = new FBComponent();
   }
   */
+
   public function beforeFilter()
   {
     $this->FB = new FBComponent();
@@ -25,10 +25,36 @@ class GameController extends AppController {
         )));
     }
   }
-
-  public function display() {
+ 
+  private function getRandomFriends() {
     $fb_friends = $this->FB->getFriends();
-    $this->set('friends', $fb_friends);
+    //gen random number
+    $fnum = count($fb_friends);
+    $f1 = rand(1, count($fnum));
+    $f2 = -1;
+    while ($f2 != $f1) $f2 = rand(count($fnum));
+
+    //return friends info
+    return array( $fb_friends[$f1],
+                  $fb_friends[$f2]);
+  }
+ 
+  private function setDataToDisp() {
+    $correctans = rand(1,2);
+    $friends = $this->getRandomFriends();
+    $statuses = $this->FB->getStatuses($friends[$correctans]['id']);
+    
+    $snum = count($statuses);
+    $sindex = rand(1, $snum);
+
+    $correctstas = $statuses[$sindex];
+  
+    return array("friends" => $friends, "type" => "status", "data" => $correctans );
+  }
+  
+  public function display() {
+    $data = $this->setDataToDisp(); 
+    $this->set('data', $data);
     $this->render('/Game/index');
   }
 }
--- app/View/Game/index.ctp
+++ app/View/Game/index.ctp
@@ -1,6 +1,5 @@
 <h1>Tomojisho</h1>
 
 <?php
-  var_dump($friends['data'][0]).'<br/>';
-  //var_dump($friendStatus).'<br/>';
+  var_dump($data);
 ?>
