d3b6ecc239a72e346023794cba798e94f64c8a44
dxhuy1988 <doxuanhuy@gmail.com>
2012-12-20 12:02:43 +0900

    debug js

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -26,7 +26,8 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
 	#Router::connect('/debug', array('controller' => 'pages', 'action' => 'display', 'home'));
-  Router::connect('/', array('controller' => 'game', 'action' => 'welcome'));
+  #Router::connect('/', array('controller' => 'game', 'action' => 'welcome'));
+  Router::connect('/', array('controller' => 'game', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
